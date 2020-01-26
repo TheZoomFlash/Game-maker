e8ec0aab2a2e4eb8aea0742b0c5643bf7339bfc3
TheZoomFlash <lin844729085@qq.com>
2020-01-26 23:37:49 +0000

    clean

--- 2D_Iso_Project/Assets/Scripts/GamePlay/Damageable.cs
+++ 2D_Iso_Project/Assets/Scripts/GamePlay/Damageable.cs
@@ -16,36 +16,37 @@ public class Damageable : MonoBehaviour
     public class HealEvent : UnityEvent<int, Damageable>
     { }
 
-    public int startingHealth = 5;
+    public int maxHealeh = 5;
+    protected int m_CurrentHealth;
+    public int CurrentHealth { get { return m_CurrentHealth; } }
+    protected Vector2 m_DamageDirection;
+    public Vector2 DamageDirection { get { return m_DamageDirection; } }
+
     public bool invulnerableAfterDamage = true;
     public float invulnerabilityDuration = 3f;
-    public bool disableOnDeath = false;
+    protected float m_InulnerabilityTimer;
+    protected bool m_Invulnerable = false;
+    public void DisableInvulnerability() => m_Invulnerable = false;
+    public void EnableInvulnerability(bool ignoreTimer = false)
+    {
+        m_Invulnerable = true;
+        //technically don't ignore timer, just set it to an insanly big number. Allow to avoid to add more test & special case.
+        m_InulnerabilityTimer = ignoreTimer ? float.MaxValue : invulnerabilityDuration;
+    }
+    
     [Tooltip("An offset from the obejct position used to set from where the distance to the Damager is computed")]
     public Vector2 centreOffset = new Vector2(0f, 1f);
+    public bool disableOnDeath = false;
+
+    //[HideInInspector]
     public HealthEvent OnHealthSet;
     public DamageEvent OnTakeDamage;
     public DamageEvent OnDie;
     public HealEvent OnGainHealth;
-    //[HideInInspector]
-
-    protected bool m_Invulnerable;
-    protected float m_InulnerabilityTimer;
-    protected int m_CurrentHealth;
-    protected Vector2 m_DamageDirection;
-    protected bool m_ResetHealthOnSceneReload;
-
-    public int CurrentHealth
-    {
-        get { return m_CurrentHealth; }
-    }
 
     void Awake()
     {
-        m_CurrentHealth = startingHealth;
-
-        OnHealthSet.Invoke(this);
-
-        DisableInvulnerability();
+        SetHealth(maxHealeh);
     }
 
     void FixedUpdate()
@@ -61,26 +62,10 @@ public class Damageable : MonoBehaviour
         }
     }
 
-    public void EnableInvulnerability(bool ignoreTimer = false)
-    {
-        m_Invulnerable = true;
-        //technically don't ignore timer, just set it to an insanly big number. Allow to avoid to add more test & special case.
-        m_InulnerabilityTimer = ignoreTimer ? float.MaxValue : invulnerabilityDuration;
-    }
-
-    public void DisableInvulnerability()
-    {
-        m_Invulnerable = false;
-    }
-
-    public Vector2 GetDamageDirection()
-    {
-        return m_DamageDirection;
-    }
 
     public void TakeDamage(Damager Damager, bool ignoreInvincible = false)
     {
-        Debug.Log(transform.name + " TakeDamage");
+        //Debug.Log(transform.name + " TakeDamage");
         if ((m_Invulnerable && !ignoreInvincible) || m_CurrentHealth <= 0)
             return;
 
@@ -88,51 +73,38 @@ public class Damageable : MonoBehaviour
         //We still want the callback that we were hit, but not the damage to be removed from health.
         if (!m_Invulnerable)
         {
-            m_CurrentHealth -= Damager.damage;
-            OnHealthSet.Invoke(this);
+            SetHealth(m_CurrentHealth - Damager.damage);
         }
 
         m_DamageDirection = transform.position + (Vector3)centreOffset - Damager.transform.position;
 
         OnTakeDamage.Invoke(Damager, this);
-
-        if (m_CurrentHealth <= 0)
-        {
-            OnDie.Invoke(Damager, this);
-            m_ResetHealthOnSceneReload = true;
-            EnableInvulnerability();
-            if (disableOnDeath) gameObject.SetActive(false);
-        }
-    }
-
-    public void GainHealth(int amount)
-    {
-        m_CurrentHealth += amount;
-
-        if (m_CurrentHealth > startingHealth)
-            m_CurrentHealth = startingHealth;
-
-        OnHealthSet.Invoke(this);
-
-        OnGainHealth.Invoke(amount, this);
     }
 
     public void SetHealth(int amount)
     {
-        m_CurrentHealth = amount;
+        m_CurrentHealth = Mathf.Clamp(amount, 0, maxHealeh);
+        OnHealthSet.Invoke(this);
 
+        CheckDie();
+    }
+
+    public void GainHealth(int amount)
+    {
+        SetHealth(m_CurrentHealth + amount);
+        OnGainHealth.Invoke(amount, this);
+    }
+
+    public void CheckDie()
+    {
         if (m_CurrentHealth <= 0)
         {
             OnDie.Invoke(null, this);
-            m_ResetHealthOnSceneReload = true;
             EnableInvulnerability();
-            if (disableOnDeath) gameObject.SetActive(false);
+            if (disableOnDeath)
+                gameObject.SetActive(false);
         }
-
-        OnHealthSet.Invoke(this);
     }
-
-      
 
 
 }
--- 2D_Iso_Project/Assets/Scripts/GamePlay/Damager.cs
+++ 2D_Iso_Project/Assets/Scripts/GamePlay/Damager.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Damager : MonoBehaviour
 {
@@ -13,34 +16,40 @@ public class Damager : MonoBehaviour
     public class NonDamagableEvent : UnityEvent<Damager>
     { }
 
-    //call that from inside the onDamagerHIt or OnNonDamagerHit to get what was hit.
-    public Collider2D LastHit { get { return m_LastHit; } }
-
     public int damage = 1;
     public float damageRate = 1f;
     private float nextDamageTimer = 0f;
 
-    public Vector2 offset = new Vector2(1.5f, 1f);
-    public Vector2 size = new Vector2(2.5f, 1f);
-    [Tooltip("SpriteRenderer used to read the flipX value used by offset Based OnSprite Facing")]
-    public SpriteRenderer spriteRenderer;
+    // range
+    public float offset = 0.75f;
+    public Vector2 size = new Vector2(1.5f, 1.0f);
+
+    //[Tooltip("SpriteRenderer used to read the flipX value used by offset Based OnSprite Facing")]
+    //public SpriteRenderer spriteRenderer;
+
     [Tooltip("If disabled, Damager ignore trigger when casting for damage")]
     public bool canHitTriggers;
-    public bool disableDamageAfterHit = false;
+    public LayerMask hittableLayers;
+
     [Tooltip("If set, an invincible Damager hit will still get the onHit message (but won't loose any life)")]
     public bool ignoreInvincibility = false;
-    public LayerMask hittableLayers;
-    public DamagableEvent OnDamageableHit;
-    public NonDamagableEvent OnNonDamageableHit;
+    //public bool disableDamageAfterHit = false;
 
     protected bool m_CanDamage = true;
+    public bool IsCanDamage { get { return m_CanDamage; } }
     public void EnableDamage() => m_CanDamage = true;
     public void DisableDamage() => m_CanDamage = false;
 
     protected ContactFilter2D m_AttackContactFilter;
     protected Collider2D[] m_AttackOverlapResults = new Collider2D[10];
     protected Transform m_DamagerTransform;
+
+    //call that from inside the onDamagerHIt or OnNonDamagerHit to get what was hit.
     protected Collider2D m_LastHit;
+    public Collider2D LastHit { get { return m_LastHit; } }
+
+    public DamagableEvent OnDamageableHit;
+    //public NonDamagableEvent OnNonDamageableHit;
 
     void Awake()
     {
@@ -53,60 +62,54 @@ public class Damager : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!m_CanDamage)
-            return;
-
         if (nextDamageTimer > 0)
         {
             nextDamageTimer -= Time.deltaTime;
             return;
         }
-        
-        if (Input.GetMouseButtonDown(0))
-        {
-            Attack();
-            nextDamageTimer = damageRate;
-        }
-             
     }
 
-    void Attack()
+    public void Attack(Vector2 dir)
     {
-        Vector2 mousePos = CameraController.Instance.GameplayCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 dir = (mousePos - (Vector2)m_DamagerTransform.position).normalized;
+        if (!IsCanDamage || nextDamageTimer > 0)
+            return;
+
+        nextDamageTimer = damageRate;
 
         Vector2 scale = m_DamagerTransform.lossyScale;
         Vector2 facingOffset = Vector2.Scale(offset * dir, scale);
-        Vector2 scaledSize = Vector2.Scale(size, scale);
+        Vector2 scaledSize = Vector2.Scale(size * dir, scale);
 
         Vector2 pointA = (Vector2)m_DamagerTransform.position + facingOffset - scaledSize * 0.5f;
         Vector2 pointB = pointA + scaledSize;
         int hitCount = Physics2D.OverlapArea(pointA, pointB, m_AttackContactFilter, m_AttackOverlapResults);
 
-        Debug.Log("Damage : " + dir + " , hitCount : " + hitCount);
+        //Debug.Log("Damage : " + dir + " , hitCount : " + hitCount);
         for (int i = 0; i < hitCount; i++)
         {
             m_LastHit = m_AttackOverlapResults[i];
             Damageable damageable = m_LastHit.GetComponentInParent<Damageable>();
-
             if (damageable)
             {
                 OnDamageableHit.Invoke(this, damageable);
                 damageable.TakeDamage(this, ignoreInvincibility);
-                if (disableDamageAfterHit)
-                    DisableDamage();
-            }
-            else
-            {
-                OnNonDamageableHit.Invoke(this);
+
+                //if (disableDamageAfterHit)
+                //    DisableDamage();
             }
         }
+        //OnNonDamageableHit.Invoke(this);
     }
 
 #if UNITY_EDITOR
     private void OnDrawGizmosSelected()
     {
-        
+        Vector3 dir = new Vector3(1.0f, 0, 0);
+        Vector3 _offset = offset * dir;
+        Vector3 _center = transform.position + _offset;
+
+        Handles.color = new Color(1.0f, 0, 0, 0.2f);
+        Handles.DrawWireCube(_center, size);
     }
 #endif
 
