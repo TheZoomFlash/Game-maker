cb34939182af51d100f11cd521c75fa7771e060b
neo <lin844729085@qq.com>
2020-01-29 17:48:14 +0000

    add loading

--- 2D_Iso_Project/Assets/Scripts/GamePlay/GameManager.cs
+++ 2D_Iso_Project/Assets/Scripts/GamePlay/GameManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    //Reference
+    public Animator scene_animator;
+    public float fadeTime = 1f;
 
     void Awake()
     {
@@ -12,6 +17,31 @@ public class GameManager : MonoBehaviour
         Application.targetFrameRate = 60;
     }
 
+    //temp
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            LoadNextScene();
+        }
+    }
 
+    public void LoadScene(int index)
+    {
+        StartCoroutine(LoadLevel(index));
+    }
 
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        scene_animator.SetTrigger("start");
+
+        yield return new WaitForSeconds(fadeTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
 }
